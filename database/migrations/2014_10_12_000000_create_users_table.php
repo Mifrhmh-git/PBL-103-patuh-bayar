51deb4a8d9cd6ee257478caea55e2deacf30a144
Mifrhmh-git <miftahurrhmh461@gmail.com>
2024-12-29 10:01:03 +0700

    add role and image fields to users table; insert default user data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,15 +12,49 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Membuat tabel users
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('role', 10);
+            $table->string('image')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('pw', 30)->nullable();
+            $table->string('password')->nullable();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
+
+        // Menambahkan data default ke tabel users
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => 'Bendahara RT',
+                'role' => 'bendahara',
+                'image' => null,
+                'email' => 'bendahara-rt@gmail.com',
+                'email_verified_at' => '2024-12-01 14:27:18',
+                'pw' => 'bendahara-rt',
+                'password' => null,
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Ketua RW',
+                'role' => 'rw',
+                'image' => null,
+                'email' => 'rw@gmail.com',
+                'email_verified_at' => '2024-12-12 06:48:06',
+                'pw' => 'rw-1',
+                'password' => null,
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 
     /**
